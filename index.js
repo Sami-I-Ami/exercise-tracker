7174a5314f510e4f1be217dec505c62724758481
Sami Fink <fink.s.m.8651@gmail.com>
2024-04-26 01:29:14 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
-
+// user endpoint
+app.route('/api/users')
+  .post(function(req, res) {
+    // output
+    res.json({
+      username: "placeholder",
+      _id: "placeholder"
+    })
+  })
 
 
 
